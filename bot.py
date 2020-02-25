11348a2971527d651919695de4792ad09d1b3239
PaulGOUX27 <paul.goux@wanadoo.fr>
2020-02-25 19:43:20 +0100

    WIP

--- bot.py
+++ bot.py
@@ -1,22 +1,46 @@
 import os
 import discord
 import yaml
+from pyBurger import *
 
 from discord.ext import commands
+from collections import defaultdict
 
 config = yaml.safe_load(open("config.yml"))
 token = config["token"]
 
-client = discord.Client()
+emojiDic = defaultdict(str)
+emojiDic.update(zip("pizza burger".split(), "🍕 🍔".split()))
 
-bot = commands.Bot(command_prefix='$')
+client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user.name} has connected to Discord!')
+    print('{client.user.name} has connected to Discord!')
+
+bot = commands.Bot(command_prefix='$')
+
+def extractEmote(string):
+	#Du python de haut nuveau, par un eskérien
+	return ''.join(emoji for key, emoji in emojiDic.items() if key in string.lower())
 
 @bot.command(name='menu', help='Affiche le menu du RU de Jussieu')
 async def menu(ctx):
-    await ctx.send("Mathieu on need ton code")
+	menu = getMenuCurrentDay()
+	string = "Repas du " + menu.date + "\r\n"
+	string += "Plat principal \r\n"
+	for plat in menu.plats:
+		string += " - "
+		string += plat
+		string += extractEmote(plat)
+		string += "\r\n"
+
+	string += "Desserts : \r\n"
+	for dessert in menu.desserts:
+		string += " - "
+		string += dessert
+		string += "\r\n"
+	await ctx.send(string)
 
 bot.run(token)
+
